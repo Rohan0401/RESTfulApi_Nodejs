a729f7e1167fbe4142a452cd10ebc327d7522d23
Rohan Rajput <rohan@ownedoutcomes.com>
2018-04-20 17:05:53 -0700

    Adding routes and mongo

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express'),
 app = express(),
-port = process.env.PORT || 3000;
+port = process.env.PORT || 3000,
+mongoose = require('mongoose'),
+Task = require('./api/models/todoListModel') ; // Model is created here
+bodyParser = require('body-parser');
+
+
+// Mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes') // importing routes
+routes(app); //register route
 
 app.listen(port);
 
